9d5cc036bcbcf8de99696db29b57c6d3f3eba08f
Alex P <>
2018-12-20 13:36:14 +0100

    added second vhost

--- start_local.sh
+++ start_local.sh
@@ -6,13 +6,20 @@
 #cp build/develop/wp-config.php public/wordpress
 
 the_host=multisite.test
+the_host2=site1.multisite.test
 
 docker-compose up -d --build
 
-if grep -q $the_host /etc/hosts; then
-    echo "Host entry exists"
-else
-    echo "127.0.0.1 $the_host"  | sudo tee -a /etc/hosts > /dev/null
-    echo "Added new host entry"
-fi
+make_host_entry() {
+    if grep -q $1 /etc/hosts; then
+        echo "Host entry exists"
+    else
+        echo "127.0.0.1 $1"  | sudo tee -a /etc/hosts > /dev/null
+        echo "Added new host entry"
+    fi
+}
+
+make_host_entry $the_host
+make_host_entry $the_host2
+
 echo "Webserver started at http://$the_host"
